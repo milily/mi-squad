6a910d744a1caf2e70ead7c6746ee22533497740
mily rivas <mrise.translations@gmail.com>
2017-05-08 00:28:30 -0300

    ejercicio sin mostrar info de mi squad

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,19 @@ function MiembroSquad(nombre, edad, hobby, hobby1, hobby2){
 	this.hobby1 = hobby1;
 	this.hobby2 = hobby2;
 	this.miSquad = function(){
-		document.write("<b>" + "Nombre: " + "</b>" + this.nombre + "<br>" + "<b>" + "Edad: " + "</b>" +
-		 this.edad + "<br>" + "<b>" + "Hobbies: " + "</b>" + "<ul>" + "<li>" + this.hobby +"</li>"+ "<li>" +this.hobby1 + "</li>" + "<li>" +this.hobby2 + "</li>" +"<br>" + "</br>" + "</ul>");
+		return("<b>" + "Nombre: " + "</b>" + this.nombre) /*+ "<br>" + "<b>" + "Edad: " + "</b>" +
+		 this.edad + "<br>" + "<b>" + "Hobbies: " + "</b>" + "<ul>" + "<li>" + this.hobby +"</li>"+ "<li>" +this.hobby1 + "</li>" + "<li>" +this.hobby2 + "</li>" +"<br>" + "</br>" + "</ul>");*/
 	};
 
 }
- var dany = new MiembroSquad("Daniela Risco", "35", "Ver peliculas", "Dormir", "Comer");
- var marce = new MiembroSquad("Marcela 'Unicornio' Orellana", "31", "Andar en bici", "Ver Doramas", "Comer");
- var angy = new MiembroSquad("Angela Aliaga", "32", "Jugar Donkey Kong Country", "Leer", "Escuchar Música");
- var gaby = new MiembroSquad("Gabriela Bravo", "28", "Ver animes", "Pintar", "Jugar Aion" );
- var paty = new MiembroSquad("Patricia Zavala", "28", "Cantar", "Comer", "Comer");
- var mily = new MiembroSquad("Mily Rivas", "18 recién cumplidos", "Ver peliculas", "Leer", "Comer")
+
+ var grupo = []; 
+ grupo.push(new MiembroSquad("Daniela Risco", "35", "Ver peliculas", "Dormir", "Comer"));
+ grupo.push(new MiembroSquad("Marcela 'Unicornio' Orellana", "31", "Andar en bici", "Ver Doramas", "Comer"));
+ grupo.push(new MiembroSquad("Angela Aliaga", "32", "Jugar Donkey Kong Country", "Leer", "Escuchar Música"));
+ grupo.push(new MiembroSquad("Gabriela Bravo", "28", "Ver animes", "Pintar", "Jugar Aion"));
+ grupo.push(new MiembroSquad("Patricia Zavala", "28", "Cantar", "Comer", "Comer"));
+ grupo.push(new MiembroSquad("Mily Rivas", "18 recién cumplidos", "Ver peliculas", "Leer", "Comer"));
  
 
  dany.miSquad();
@@ -24,3 +26,8 @@ function MiembroSquad(nombre, edad, hobby, hobby1, hobby2){
  gaby.miSquad();
  paty.miSquad();
  mily.miSquad();
+
+ mostrar(){
+	("<b>" + "Nombre: " + "</b>" + this.nombre + "<br>" + "<b>" + "Edad: " + "</b>" +
+		 this.edad + "<br>" + "<b>" + "Hobbies: " + "</b>" + "<ul>" + "<li>" + this.hobby +"</li>"+ "<li>" +this.hobby1 + "</li>" + "<li>" +this.hobby2 + "</li>" +"<br>" + "</br>" + "</ul>");
+ } 
